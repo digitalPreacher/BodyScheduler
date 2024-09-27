a742e8ee0c03ec4bada83ecc38b6ff0f48b1e254
Николай Пронин <Proninkolian@yandex.ru>
2024-09-27 23:31:21 +0300

    Added DTOs

---
+++ BodyShedule_v_2_0.Server/DataTransferObjects/SignInDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BodyShedule_v_2_0.Server.DataTransferObjects
+{
+    public class SignInDTO
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+    }
+}
