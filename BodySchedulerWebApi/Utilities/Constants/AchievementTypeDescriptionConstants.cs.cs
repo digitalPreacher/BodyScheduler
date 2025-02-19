7085d1d9264de3c35b248793f4080ba922ed4165
Nikolai <proninkolian@yandex.ru>
2025-02-19 22:33:18 +0300

    Change achievement constants

--- BodySchedulerWebApi/Utilities/Constants/AchievementTypeDescriptionConstants.cs.cs
+++ BodySchedulerWebApi/Utilities/Constants/AchievementTypeDescriptionConstants.cs.cs
@@ -2,11 +2,11 @@
 {
     public class AchievementTypeDescriptionConstants
     {
-        public const string BeginnerDescription = "Завершена 1 тренировка";
-        public const string YoungDescription = "Завершены  10 тренировок";
-        public const string СontinuingDescription = "Завершены  50 тренировок";
-        public const string AthleteDescription = "Завершены  100 тренировок";
-        public const string UniverseDescription = "Завершены  500 тренировок";
-        public const string StartedDescription = "Добавить первую тренировку";
+        public const string BeginnerDescription = "Завершите 1 тренировку";
+        public const string YoungDescription = "Завершите  10 тренировок";
+        public const string СontinuingDescription = "Завершите  50 тренировок";
+        public const string AthleteDescription = "Завершите  100 тренировок";
+        public const string UniverseDescription = "Завершите  500 тренировок";
+        public const string StartedDescription = "Добавьте первую тренировку";
     }
 }
